40e528051e1f29ec30df1c41aa22d96ecab3ef48
atefBB <atef.bettaib@gmail.com>
2016-11-12 18:43:26 +0100

    try to solve prob while saving sended image to local storage

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,31 @@ Route::get('/', function () {
 Route::post('save_img', function() {
   $captured_image = Request::get('data');
   $d = new Carbon\Carbon();
-  $img_save_path = storage_path().'/app/imgs/img_'.$d->toDateString().'.png';
-  dd(file_put_contents($img_save_path, $captured_image));
+  //$img_save_path = storage_path().'/app/imgs/img_'.$d->toDateString().'.png';
+  //dd(file_put_contents($img_save_path, $captured_image));
+
+      if (
+          preg_match(
+              '/data:image\/(gif|jpeg|png);base64,(.*)/i',
+              $captured_image,
+              $matches))
+              {
+          $imageType = $matches[1];
+          $imageData = base64_decode($matches[2]);
+          $image = imagecreatefromstring($imageData);
+          $filename = 'img_'.$d->toDateString().'.png';
+
+          if (
+                imagepng(
+                  $image,
+                  storage_path().'/app/imgs/'. $filename))
+          {
+              echo json_encode(
+                      array('filename' => '/app/imgs/' . $filename));
+          } else {
+              /*throw new Exception*/echo ('Could not save the file.');
+          }
+      } else {
+          /*throw new Exception*/echo ('Invalid data URL.');
+      }
 });
